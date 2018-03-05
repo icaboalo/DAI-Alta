11d14e88c32996e6459a3c398b4b44e566fe19b8
Iñaki Cabo Alonso <icaboalo@gmail.com>
2018-03-05 12:58:10 -0600

    Add files via upload

---
+++ WpfApplication1/WpfApplication1/Connection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApplication1
+{
+    class Connection
+    {
+        public static SqlConnection addConnection() {
+            try {
+                SqlConnection connection = new SqlConnection("Data Source=CC102-28;Initial Catalog=registro2;Persist Security Info=True;User ID=sa;Password=sqladmin");
+                connection.Open();
+                return connection;
+            } catch (Exception e) {
+                MessageBox.Show("No se pudo conectar a la base de datos.");
+                return null;
+            }
+        }
+    }
+}
---
+++ WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            SqlConnection connection = Connection.addConnection();
+            if (connection != null) {
+                SqlCommand command = new SqlCommand(String.Format("SELECT contra FROM usuarios WHERE nombreUsuario = '{0}'", txUsuario.Text), connection);
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read()) {
+                    if (txPassword.Text == reader.GetString(0)) {
+                        MessageBox.Show("Inicio de sesión completo!");
+                        Window alta = new Alta();
+                        alta.Show();
+                        this.Close();                     
+                    } else {
+                        MessageBox.Show("Credenciales invalidas");
+                    }
+                }
+            }
+        }
+    }
+}
